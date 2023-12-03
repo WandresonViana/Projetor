f8cbb43e535228c4511c021bbadd6447d5848d10
Wandreson Viana <wandresonviana01@gmail.com>
2023-12-03 14:48:33 -0300

    Updates

---
+++ projetor/src/model/DataShow.java
@@ -0,0 +1,29 @@
+public class DataShow {
+    private int id;
+    private String descircao;
+
+    public DataShow() {
+    }
+
+    public DataShow(int id, String descircao) {
+        this.id = id;
+        this.descircao = descircao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescircao() {
+        return descircao;
+    }
+
+    public void setDescircao(String descircao) {
+        this.descircao = descircao;
+    }
+
+}
